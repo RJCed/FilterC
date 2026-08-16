088975130a787daedb0403377ed4096d9b8c9bb3
Arjay Cedigo <256202735+RJCed@users.noreply.github.com>
2026-08-16 13:26:48 +0800

    store each pixel value to a 2d array

--- filter.c
+++ filter.c
@@ -22,7 +22,12 @@ typedef struct __attribute__((packed))
 } InfoHeader;
 
 
-
+typedef struct
+{
+    unsigned char blue;
+    unsigned char green;
+    unsigned char red;
+} Pixel;
 
 int main(int argc, char *argv[])
 {
@@ -62,6 +67,8 @@ int main(int argc, char *argv[])
     fread(&headerInfo, sizeof(headerInfo), 1, inputFile);
 
 
+
+
     // Copy header and headerInfo to the output file
     fwrite(&header, sizeof(header), 1, outputFile);
     fwrite(&headerInfo, sizeof(headerInfo), 1, outputFile);
@@ -71,29 +78,67 @@ int main(int argc, char *argv[])
     int rowPadding = (4 - ((headerInfo.width * bytesPerPixel) % 4)) % 4;
     
 
+
+
     int height = abs(headerInfo.height);
     int width = headerInfo.width;
-
-
-    // Loop through each pixel
-    unsigned char pixel[3];
-    unsigned char padding[3];
-    for (int i = 0; i < height; i++) // EACH ROW
+    
+    // Allocate memory for the row pointers
+    Pixel **image = malloc(height * sizeof(Pixel *));
+    if (image == NULL)
     {
-        for (int j = 0; j < width; j++) // EACH PIXEL
+        return 1;
+    }
+
+    // Allocate memory for each pixels
+    for (int i = 0; i < height; i++)
+    {
+        image[i] = malloc(width * sizeof(Pixel));
+        if (image[i] == NULL)
+        {
+            return 1;
+        }
+    }
+
+
+
+    // Copy each pixel to the image array
+    unsigned char pixel[3];
+    unsigned char padding[3] = {0};
+    for (int i = 0; i < height; i++) // EACH ROW INPUT
+    {
+        for (int j = 0; j < width; j++) // EACH PIXEL INPUT
         {
             // Read BGR value of each pixel
             fread(pixel, 1, 3, inputFile);  
-            
-            // Modify pixel
 
-            fwrite(pixel, 1, 3, outputFile);
+            image[i][j].blue = pixel[0];
+            image[i][j].green = pixel[1];
+            image[i][j].red = pixel[2];
+
         }
         // Padding bytes at the end of each row
-        fread(padding, 1, rowPadding, inputFile);
-        fwrite(padding, 1, rowPadding, outputFile);
+        fseek(inputFile, rowPadding, SEEK_CUR);
     }
 
+
+
+    // MODIFY EACH PIXEL
+
+
+
+    // Copy each pixel in the image array to the output
+    for (int i = 0; i < height; i++) // EACH ROW OUTPUT
+    {
+        for (int j = 0; j < width; j++) // EACH PIXEL OUTPUT
+        {
+            // Read BGR value of each pixel
+            fwrite(&image[i][j], sizeof(Pixel), 1, outputFile);
+
+        }
+        // Padding bytes at the end of each row
+        fwrite(padding, 1, rowPadding, outputFile);
+    }
 
 
 
@@ -102,4 +147,11 @@ int main(int argc, char *argv[])
     // Close Files
     fclose(inputFile);
     fclose(outputFile);
+
+    // Free allocated memory for pixels
+    for (int i = 0; i < height; i++)
+    {
+        free(image[i]);
+    }
+    free(image);
 }
