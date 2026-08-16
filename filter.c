6c038bc90b964b8528fa25ccd613d4f715a5e76f
Arjay Cedigo <256202735+RJCed@users.noreply.github.com>
2026-08-16 09:57:04 +0800

    add looper through each pixel

--- filter.c
+++ filter.c
@@ -12,6 +12,17 @@ typedef struct __attribute__((packed))
     unsigned int offset;
 } FileHeader;
 
+typedef struct __attribute__((packed))
+{
+    unsigned int size;
+    unsigned int width;
+    int height;
+    unsigned short planes;
+    unsigned short bitsPixel;
+} InfoHeader;
+
+
+
 
 int main(int argc, char *argv[])
 {
@@ -42,12 +53,51 @@ int main(int argc, char *argv[])
         return 2;
     }
 
-    // Store the input file header
+    // Store the input file FileHeader
     FileHeader header;
     fread(&header, sizeof(header), 1, inputFile);
 
-    printf("Pixel data starts at %u\n", header.offset);
-    printf("Size of FileHeader: %zu\n", sizeof(FileHeader));
+    // Store the input file InfoHeader
+    InfoHeader headerInfo;
+    fread(&headerInfo, sizeof(headerInfo), 1, inputFile);
+
+
+    // Copy header and headerInfo to the output file
+    fwrite(&header, sizeof(header), 1, outputFile);
+    fwrite(&headerInfo, sizeof(headerInfo), 1, outputFile);
+
+    // Account for row padding in case row size (width * bytes per pixel) is not divisible by 4
+    int bytesPerPixel = headerInfo.bitsPixel / 8;
+    int rowPadding = (4 - ((headerInfo.width * bytesPerPixel) % 4)) % 4;
+    
+
+    int height = abs(headerInfo.height);
+    int width = headerInfo.width;
+
+
+    // Loop through each pixel
+    unsigned char pixel[3];
+    unsigned char padding[3];
+    for (int i = 0; i < height; i++) // EACH ROW
+    {
+        for (int j = 0; j < width; j++) // EACH PIXEL
+        {
+            // Read BGR value of each pixel
+            fread(pixel, 1, 3, inputFile);  
+            
+            // Modify pixel
+
+            fwrite(pixel, 1, 3, outputFile);
+        }
+        // Padding bytes at the end of each row
+        fread(padding, 1, rowPadding, inputFile);
+        fwrite(padding, 1, rowPadding, outputFile);
+    }
+
+
+
+
+    printf("Width: %u Height: %i Bits per Pixel: %hu\n", headerInfo.width, headerInfo.height, headerInfo.bitsPixel);
 
     // Close Files
     fclose(inputFile);
